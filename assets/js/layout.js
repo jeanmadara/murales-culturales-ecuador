591150ee9015069eaca40600b818cb145feac57a
jeanmadara <jean_car1993@hotmail.com>
2026-07-01 22:37:53 -0500

    feat: implement museum-grade aesthetic system and interactive scrollytelling layout with custom Optima-Bold typography

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -10,9 +10,6 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="${basePath}assets/css/style.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet">
     `;
 
     document.head.insertAdjacentHTML('beforeend', headContent);
